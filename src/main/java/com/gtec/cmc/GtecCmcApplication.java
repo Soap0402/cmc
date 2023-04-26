0ace3bf3118b7d8753d8250d7441c27df151350e
Soap0402 <soapt@kakao.com>
2023-04-26 19:36:40 +0900

    Initial commit

---
+++ src/main/java/com/gtec/cmc/GtecCmcApplication.java
@@ -0,0 +1,13 @@
+package com.gtec.cmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtecCmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtecCmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtec/cmc/GtecCmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtec.cmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtecCmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
